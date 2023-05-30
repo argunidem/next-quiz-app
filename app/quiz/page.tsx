ba1252023f66cfb013cc3e0b429c83023105300d
argunidem <argun.idem@hotmail.com>
2023-05-30 16:10:15 +0200

    add answer selection and result logging

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -5,9 +5,11 @@ import { quiz } from '../data';
 
 const page = () => {
   const [activeQuestion, setActiveQuestion] = useState(0);
-  const [selectedAnswer, setSelectedAnswer] = useState('');
+  const [selectedAnswer, setSelectedAnswer] = useState(false);
   const [checked, setChecked] = useState(false);
-  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState<null | number>(
+    null
+  );
   const [showResult, setShowResult] = useState(false);
   const [result, setResult] = useState({
     score: 0,
@@ -18,6 +20,18 @@ const page = () => {
   const { questions, totalQuestions } = quiz;
   const { question, answers, correctAnswer } = questions[activeQuestion];
 
+  const onAnswerSelected = (answer: string, idx: number) => {
+    setChecked(true);
+    setSelectedAnswerIndex(idx);
+    if (answer === correctAnswer) {
+      setSelectedAnswer(true);
+      console.log('true');
+    } else {
+      setSelectedAnswer(false);
+      console.log('false');
+    }
+  };
+
   return (
     <div className='container'>
       <h1>Quiz Page</h1>
@@ -26,6 +40,26 @@ const page = () => {
           Question: {activeQuestion + 1}
           <span>/{totalQuestions}</span>
         </h2>
+      </div>
+      <div>
+        {!showResult ? (
+          <div className='quiz-container'>
+            <h3>{questions[activeQuestion].question}</h3>
+            {answers.map((answer, idx) => (
+              <li
+                key={idx}
+                onClick={() => onAnswerSelected(answer, idx)}
+                className={
+                  selectedAnswerIndex === idx ? 'li-selected' : 'li-hover'
+                }
+              >
+                <span>{answer}</span>
+              </li>
+            ))}
+          </div>
+        ) : (
+          <div className='quiz-container'></div>
+        )}{' '}
       </div>
     </div>
   );
