8b9e18f7e912a30daf6c1eb0fc9c22d17728d424
argunidem <argun.idem@hotmail.com>
2023-05-30 15:54:40 +0200

    add initial structure for quiz page and data file

---
+++ app/quiz/page.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { useState } from 'react';
+import { quiz } from '../data';
+
+const page = () => {
+  const [activeQuestion, setActiveQuestion] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState('');
+  const [checked, setChecked] = useState(false);
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
+  const [showResult, setShowResult] = useState(false);
+  const [result, setResult] = useState({
+    score: 0,
+    correctAnswers: 0,
+    wrongAnswers: 0,
+  });
+
+  const { questions, totalQuestions } = quiz;
+  const { question, answers, correctAnswer } = questions[activeQuestion];
+
+  return (
+    <div className='container'>
+      <h1>Quiz Page</h1>
+      <div>
+        <h2>
+          Question: {activeQuestion + 1}
+          <span>/{totalQuestions}</span>
+        </h2>
+      </div>
+    </div>
+  );
+};
+export default page;
